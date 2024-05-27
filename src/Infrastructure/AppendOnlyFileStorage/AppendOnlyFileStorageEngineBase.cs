de3a794c70f59aba1141dcf0ce777dd70c6d4d52
Pierre G. Boutquin <pierre@boutquin.com>
2024-05-27 19:02:53 -0400

    Added file header

--- src/Infrastructure/AppendOnlyFileStorage/AppendOnlyFileStorageEngineBase.cs
+++ src/Infrastructure/AppendOnlyFileStorage/AppendOnlyFileStorageEngineBase.cs
@@ -1,4 +1,19 @@
-﻿namespace Boutquin.Storage.Infrastructure.AppendOnlyFileStorage;
+﻿// Copyright (c) 2024 Pierre G. Boutquin. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License").
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+namespace Boutquin.Storage.Infrastructure.AppendOnlyFileStorage;
 
 /// <summary>
 /// Provides a base class for append-only file-based storage engines with asynchronous operations.
@@ -23,6 +38,8 @@
 /// <para>- <see cref="GetAllItemsAsync(CancellationToken)"/>: Retrieves all key-value pairs from the store.</para>
 /// <para>- <see cref="SetBulkAsync(IEnumerable{KeyValuePair{TKey, TValue}}, CancellationToken)"/>: Sets or updates values for multiple keys.</para>
 /// </remarks>
+///
+
 public abstract class AppendOnlyFileStorageEngineBase<TKey, TValue> : ICompactableStorageEngine<TKey, TValue>
     where TKey : ISerializable<TKey>, IComparable<TKey>, new()
     where TValue : ISerializable<TValue>, new()
